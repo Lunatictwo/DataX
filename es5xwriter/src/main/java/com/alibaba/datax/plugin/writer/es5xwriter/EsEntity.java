f9ce02037be66c234c0709551c29a9d987f272ef
lunatictwo <lunatictwo@163.com>
2017-11-23 17:48:17 +0800

    change for pr.

--- es5xwriter/src/main/java/com/alibaba/datax/plugin/writer/es5xwriter/EsEntity.java
+++ es5xwriter/src/main/java/com/alibaba/datax/plugin/writer/es5xwriter/EsEntity.java
@@ -1,7 +1,25 @@
 package com.alibaba.datax.plugin.writer.es5xwriter;
 
 /**
- * Created by zehui on 2017/11/23.
+ * Created  on 2017/8/14.
+ * @author lunatictwo
  */
 public class EsEntity {
+    /**
+     * 显式声明transient 说明该字段不需要进行序列化，
+     * 否则在使用Gson的时候会和子类的id产生冲突 但是子类也需要该字段
+     * 一些敏感数据字段也可以通过该方法禁止进行序列化
+     */
+    private transient String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
 }
